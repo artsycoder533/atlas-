6b0ba6cc3ac60cac40cf2540113a5b38f1376b51
Natasha <njohnson533@gmail.com>
2022-01-19 19:54:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Freelance Template 1
+
+- [Description](#description)
+  - [Built With](#built-with)
+  - [Screenshots](#screenshots)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Challenges](#challenges)
+  - [Installation Instructions](#installation-instructions)
+- [Author](#author)
+
+## Description
+
+Freelance Template 1 is a website template I created from scratch to facilitate creating a website for freelance clients.
+
+### Built With
+- Html
+- Css
+- JavaScript
+
+### Screenshots
+
+![Desktop Screenshot]()
+![Mobile Screenshot]()
+
+### Links
+
+- Live Site URL: []()
+- Repository URL: []()
+- Video Demo on Youtube: []()
+
+## My Process
+
+
+
+### Challenges 
+
+
+
+### Installation Instructions
+To use the template:
+
+## Author
+Portfolio: [https://natashajohnson.dev/](https://natashajohnson.dev/)
---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+const hamburgerBtn = document.getElementById("hamburger");
+const line = document.getElementById("line");
+const navLinks = document.getElementById("nav-links");
+const scrollBtn = document.getElementById("scroll");
+const header = document.querySelector(".header");
+const date = document.querySelector(".date");
+const accordians = document.getElementsByClassName("accordian-icon");
+const minus = document.querySelector(".fa-minus");
+const plus = document.querySelector(".fa-plus");
+
+const toggleMenu = () => {
+    line.classList.toggle("open-hamburger");
+    navLinks.classList.toggle("show-links");
+}
+
+// sticky header
+window.addEventListener("scroll", () => {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// scroll to top
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 70) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+scrollBtn.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0;
+});
+
+//dynamic date
+const currentDate = new Date().getFullYear();
+date.textContent = currentDate;
+
+// accordian
+for (let i = 0; i < accordians.length; i++){
+  console.log("we here");
+  accordians[i].addEventListener("click", function () {
+    this.nextElementSibling.classList.toggle("accordian-active");
+    this.parentElement.nextElementSibling.classList.toggle("show-accordian");
+    minus.classList.toggle("show");
+    plus.classList.toggle("hide");
+  })
+}
+
+
+hamburgerBtn.addEventListener("click", toggleMenu);
